244d9a2021ce7279a4bff4b3f8cee9c98fd477d3
zizle <462894999@qq.com>
2020-10-15 17:34:52 +0800

    首页报告

--- modules/report_file/report.py
+++ modules/report_file/report.py
@@ -5,27 +5,22 @@
 
 """ 管理报告的API
 API-1: 网络上传报告文件
-API-2: 条件查询报告
-API-3: 删除报告
-API-4: 隐藏或公开报告
+API-2: 条件查询报告（按日期）
+API-3: 条件查询报告(分页)
+API-4: 删除报告
+API-5: 隐藏或公开报告
+API-6: 首页日报获取最新5-50个信息
 """
 import os
 from fastapi import APIRouter, Depends, Form, UploadFile, HTTPException, Query, Body
 from utils.verify import oauth2_scheme, decipher_user_token
 from db.mysql_z import MySqlZ
+from utils.constant import VARIETY_ZH, REPORT_TYPES
+from .models import ReportType
 from configs import FILE_STORAGE
 
+
 report_router = APIRouter()
-
-
-REPORT_TYPE_DICT = {
-    "daily": "每日报告",
-    "weekly": "周度报告",
-    "monthly": "月度报告",
-    "annual": "年度报告",
-    "special": "专题报告",
-    "others": "其他",
-}
 
 
 @report_router.post("/report-file/", summary="上传报告文件")
@@ -55,6 +50,7 @@ async def create_report(
     filename = report_file.filename
     if rename_text:
         filename = rename_text + ".pdf"
+    title = os.path.splitext(filename)[0]
     report_path = os.path.join(report_folder, filename)
     sql_path = os.path.join(save_folder, filename)
     # 创建数据库记录
@@ -64,9 +60,9 @@ async def create_report(
         )
         if not cursor.fetchone():
             cursor.execute(
-                "INSERT INTO research_report (`date`,creator,variety_en,report_type,filepath) "
-                "VALUES (%s,%s,%s,%s,%s);",
-                (date, user_id, relative_varieties, report_type, sql_path)
+                "INSERT INTO research_report (`date`,creator,variety_en,title,report_type,filepath) "
+                "VALUES (%s,%s,%s,%s,%s,%s);",
+                (date, user_id, relative_varieties, title, report_type, sql_path)
             )
         content = await report_file.read()      # 将文件保存到目标位置
         with open(report_path, "wb") as fp:
@@ -75,26 +71,76 @@ async def create_report(
     return {"message": "上传成功!"}
 
 
-@report_router.get("/report-file/", summary="条件查询报告信息")
+@report_router.get("/report-file/paginator/", summary="条件获取报告信息(分页)")
+async def get_report_with_paginator(
+        report_type: str = Query(...),
+        variety_en: str = Query("0"),
+        page: int = Query(1, ge=1),
+        page_size: int = Query(10, ge=10, le=1000)
+):
+    # 验证report_type:
+    if report_type not in REPORT_TYPES.keys():
+        raise HTTPException(status_code=400, detail="Unknown Report Type")
+
+    if variety_en == "0":
+        execute_sql = "SELECT id,`date`,variety_en,title,report_type,filepath,is_active FROM research_report " \
+                      "WHERE report_type=%s ORDER BY `date` DESC LIMIT %s,%s;"
+        execute_params = (report_type, (page-1)*page_size, page_size)
+        count_execute_sql = "SELECT COUNT(id) AS total_count FROM research_report " \
+                            "WHERE report_type=%s;"
+        count_execute_params = (report_type, )
+    else:
+        execute_sql = "SELECT id,`date`,variety_en,title,report_type,filepath,is_active FROM research_report " \
+                      "WHERE report_type=%s AND LOCATE(%s,variety_en) > 0 " \
+                      "ORDER BY `date` DESC LIMIT %s,%s;"
+        execute_params = (report_type, variety_en, (page-1)*page_size, page_size)
+        count_execute_sql = "SELECT COUNT(id) AS total_count FROM research_report " \
+                            "WHERE report_type=%s AND LOCATE(%s,variety_en) > 0;"
+        count_execute_params = (report_type, variety_en)
+    with MySqlZ() as cursor:
+        cursor.execute(execute_sql, execute_params)
+        reports = cursor.fetchall()
+        # 查询总量
+        cursor.execute(count_execute_sql, count_execute_params)
+        total_count = cursor.fetchone()["total_count"]
+    # 计算总页码
+    total_page = int((total_count + page_size - 1) / page_size)
+    # 处理文件名
+    for report_item in reports:
+        report_item["type_text"] = REPORT_TYPES.get(report_item["report_type"], report_item["report_type"])
+        report_item["variety_zh"] = VARIETY_ZH.get(report_item["variety_en"], report_item["variety_en"])
+
+    return {"message": "查询成功!", "reports": reports, "page": page, "page_size": page_size, "total_page": total_page}
+
+
+@report_router.get("/report-file/", summary="条件查询报告信息(按日期)")
 async def get_report_info(
         query_date: str = Query(...),
         report_type: str = Query(...),
-        variety_en: str = Query(...)
+        variety_en: str = Query('0')
 ):
     # 验证report_type:
-    if report_type not in ["daily", "weekly", "monthly", "annual", "special", "others"]:
+    if report_type not in REPORT_TYPES.keys():
         raise HTTPException(status_code=400, detail="Unknown Report Type")
     with MySqlZ() as cursor:
-        cursor.execute(
-            "SELECT id,`date`,variety_en,report_type,filepath,is_active FROM research_report "
-            "WHERE `date`=%s AND report_type=%s AND LOCATE(%s,variety_en) > 0;",
-            (query_date, report_type, variety_en)
-        )
+        if variety_en == '0':
+            cursor.execute(
+                "SELECT id,`date`,variety_en,title,report_type,filepath,is_active FROM research_report "
+                "WHERE `date`=%s AND report_type=%s;",
+                (query_date, report_type)
+            )
+        else:
+            cursor.execute(
+                "SELECT id,`date`,variety_en,title,report_type,filepath,is_active FROM research_report "
+                "WHERE `date`=%s AND report_type=%s AND LOCATE(%s,variety_en) > 0;",
+                (query_date, report_type, variety_en)
+            )
         reports = cursor.fetchall()
     # 处理文件名
     for report_item in reports:
         report_item["filename"] = os.path.split(report_item["filepath"])[1]
-        report_item["type_text"] = REPORT_TYPE_DICT.get(report_item["report_type"], '')
+        report_item["type_text"] = REPORT_TYPES.get(report_item["report_type"], report_item["report_type"])
+        report_item["variety_zh"] = VARIETY_ZH.get(report_item["variety_en"], report_item["variety_en"])
     return {"message": "查询成功!", "reports": reports}
 
 
@@ -147,3 +193,20 @@ async def delete_report_file(
         if os.path.exists(report_path) and os.path.isfile(report_path):
             os.remove(report_path)
     return {"message": "删除成功!"}
+
+
+@report_router.get("/latest-report/", summary="灵活获取最新5-50个报告")
+async def get_report(report_type: ReportType = Query(...), count: int = Query(..., ge=5, le=50)):
+    with MySqlZ() as cursor:
+        cursor.execute(
+            "SELECT `date`,variety_en,title,report_type,filepath "
+            "FROM research_report "
+            "WHERE report_type=%s AND is_active=1 "
+            "ORDER BY `date` DESC LIMIT %s;",
+            (report_type, count)
+        )
+        reports = cursor.fetchall()
+    for report_item in reports:
+        report_item["type_zh"] = REPORT_TYPES.get(report_item["report_type"], report_item["report_type"])
+        report_item["variety_zh"] = VARIETY_ZH.get(report_item["variety_en"], report_item["variety_en"])
+    return {"message": "获取最新{}成功!".format(REPORT_TYPES.get(report_type)), "reports": reports}
